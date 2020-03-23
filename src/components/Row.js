5be2141146fcdf0aded53b49c0d5b65f9efbb6db
Jeremy Winterberg <jeremy@jeremydwayne.com>
2020-03-23 04:49:15 -0500

    init

---
+++ src/components/Row.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import RowStyle from './styles/RowStyle';
+import PropTypes from 'prop-types';
+
+export default class Row extends Component {
+	static propTypes = {
+		row: PropTypes.object.isRequired
+	};
+	render() {
+		const { row } = this.props;
+		return (
+			<RowStyle className="pure-g">
+				<span className="pure-u-1-3 Row-map">{row.map}</span>
+				<span className="pure-u-1-24 Row-tier">{row.tier}</span>
+				<span className="pure-u-1-3 Row-region">{row.region}</span>
+				<span className="pure-u-1-24 Row-atlasbonus">
+					<input type="checkbox" checked={row.atlasbonus}></input>
+				</span>
+				<span className="pure-u-1-24 Row-awakeningbonus">
+					<input type="checkbox" checked={row.awakeningbonus}></input>
+				</span>
+				<span className="pure-u-1-12 Row-awakeningtier">
+					{row.awakeningtier}
+				</span>
+			</RowStyle>
+		);
+	}
+}
---
+++ src/components/styles/RowStyle.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const RowStyle = styled.div`
+	.Row-map {
+		margin: 5px 5px;
+		font-weight: 700;
+		font-size: 20px;
+	}
+	.Row-tier {
+		margin: 5px 5px;
+	}
+	.Row-region {
+		margin: 5px 5px;
+	}
+	.Row-atlasbonus {
+		margin: 5px 5px;
+	}
+	.Row-awakeningbonus {
+		margin: 5px 5px;
+	}
+	.Row-awakeningtier {
+		margin: 5px 5px;
+	}
+`;
+
+export default RowStyle;
